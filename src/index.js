5e7edfb016d40cf59ff1e4d6a7de5c48ea17fb72
Tetiana Karpenko <kuzia2016@i.ua>
2023-02-15 10:49:20 +0200

    feat: add task's solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = bracketsConfig.map(item => item.join(''));
+
+  while (bracketsConfig.length) {
+    let count = 0;
+
+    bracketsConfig.forEach(item => {
+      if (str.indexOf(item) != -1) {
+        str = str.replace(item, '');
+      } else {
+        count += 1;
+      }
+    });
+
+    if (count === bracketsConfig.length) {
+      bracketsConfig.length = 0;
+    }
+  }
+
+  if(str.length === 0) return true;
+  else return false;
 }
